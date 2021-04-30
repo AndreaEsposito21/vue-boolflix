122a7c67db170bffa1aad75f7f515e8321c1a969
Andrea Esposito <andexpox@gmail.com>
2021-04-30 16:41:01 +0200

    wip

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// VUE JS
+var app = new Vue ({
+    el: '#root',
+    data: {
+        inputBar: '',
+        movieArray: []
+    },
+    methods: {
+        inputSearchResult() {
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=8a8133740665dc34053c39e30c913e2d', {
+                params: {
+                    language: 'it-IT',
+                    query: this.inputBar,
+                    page: 1
+                }
+            })
+            .then((response) => {
+                const result = response.data;
+                this.movieArray = result.results;
+            });
+            console.log(movieArray);
+        }
+    },
+    mounted() {
+        
+    }
+});
